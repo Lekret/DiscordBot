480e199bd0af4aa35ef6b5f7607d21274fc210bf
Lekret <lenicraft@mail.ru>
2022-06-13 01:23:08 +0300

    Removed test command

--- DiscordBot/Source/Modules/CommandModule.cs
+++ DiscordBot/Source/Modules/CommandModule.cs
@@ -9,11 +9,6 @@ namespace DiscordBot.Source.Modules
 {
     public class CommandModule : ModuleBase<SocketCommandContext>
     {
-        public CommandModule(DiscordSocketClient client)
-        {
-            client.SelectMenuExecuted += Handler;
-        }
-        
         [RequireUserPermission(GuildPermission.KickMembers, ErrorMessage = "Тебе нельзя")]
         [Command("kick")]
         public async Task Kick(IUser target = null, [Remainder] string reason = "")
@@ -35,12 +30,6 @@ namespace DiscordBot.Source.Modules
             }
         }
 
-        [Command("test")]
-        public async Task Test()
-        {
-            await ExecuteOnSelectedUser("Test", null);
-        }
-
         [Command("hello")]
         public async Task Hello()
         {
@@ -49,26 +38,6 @@ namespace DiscordBot.Source.Modules
                 await ReplyAsync($"{user.Mention} Привет, создатель!");
             else
                 await ReplyAsync($"{user.Mention} Приветствую!");
-        }
-
-        private async Task ExecuteOnSelectedUser(string name, Action<IUser> execute)
-        {
-            var menuBuilder = new SelectMenuBuilder().WithCustomId("select_user");
-            foreach (var user in Context.Guild.Users)
-            {
-                menuBuilder.AddOption(user.Username, $"{user.Id}");
-            }
-
-            var componentBuilder = new ComponentBuilder()
-                .WithButton("Do", "do_button", ButtonStyle.Success)
-                .WithSelectMenu(menuBuilder);
-            await ReplyAsync(name, components: componentBuilder.Build());
-        }
-
-        private static async Task Handler(SocketMessageComponent arg)
-        {
-            var text = string.Join(", ", arg.Data.Values);
-            await arg.RespondAsync($"You have selected {text}");
         }
     }
 }
