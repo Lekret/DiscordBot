80927b7d112aec1df10eb2fd446a9625bcf3544f
Lekret <lenicraft@mail.ru>
2022-06-12 23:59:26 +0300

    Moved Application into Program

--- DiscordBot/Source/Modules/CommandModule.cs
+++ DiscordBot/Source/Modules/CommandModule.cs
@@ -1,12 +1,19 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
+using Discord.WebSocket;
 using DiscordBot.Source.Utils;
 
 namespace DiscordBot.Source.Modules
 {
     public class CommandModule : ModuleBase<SocketCommandContext>
     {
+        public CommandModule(DiscordSocketClient client)
+        {
+            client.SelectMenuExecuted += Handler;
+        }
+        
         [RequireUserPermission(GuildPermission.KickMembers, ErrorMessage = "Тебе нельзя")]
         [Command("kick")]
         public async Task Kick(IUser target = null, [Remainder] string reason = "")
@@ -27,7 +34,13 @@ namespace DiscordBot.Source.Modules
                 await ReplyAsync($"{Context.User.Mention} Ты не можешь кикнуть {target.Mention}");
             }
         }
-        
+
+        [Command("test")]
+        public async Task Test()
+        {
+            await ExecuteOnSelectedUser("Test", null);
+        }
+
         [Command("hello")]
         public async Task Hello()
         {
@@ -36,6 +49,26 @@ namespace DiscordBot.Source.Modules
                 await ReplyAsync($"{user.Mention} Привет, создатель!");
             else
                 await ReplyAsync($"{user.Mention} Приветствую!");
+        }
+
+        private async Task ExecuteOnSelectedUser(string name, Action<IUser> execute)
+        {
+            var menuBuilder = new SelectMenuBuilder().WithCustomId("select_user");
+            foreach (var user in Context.Guild.Users)
+            {
+                menuBuilder.AddOption(user.Username, $"{user.Id}");
+            }
+
+            var componentBuilder = new ComponentBuilder()
+                .WithButton("Do", "do_button", ButtonStyle.Success)
+                .WithSelectMenu(menuBuilder);
+            await ReplyAsync(name, components: componentBuilder.Build());
+        }
+
+        private static async Task Handler(SocketMessageComponent arg)
+        {
+            var text = string.Join(", ", arg.Data.Values);
+            await arg.RespondAsync($"You have selected {text}");
         }
     }
 }
--- DiscordBot/Source/Program.cs
+++ DiscordBot/Source/Program.cs
@@ -1,13 +1,67 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Net.Mime;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using DiscordBot.Source.Services;
+using DiscordBot.Source.Utils;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DiscordBot.Source
 {
-    public static class Program
+    public class Program
     {
         public static Task Main(string[] args)
         {
             var token = args[0];
-            return new Application(token).Start();
+            return new Program(token).Start();
+        }
+        
+        private readonly DiscordSocketClient _client;
+        private readonly CommandService _commands;
+        private readonly string _token;
+
+        private Program(string token)
+        {
+            _token = token;
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Info,
+                GatewayIntents = GatewayIntents.All
+            });
+            
+            _commands = new CommandService(new CommandServiceConfig
+            {
+                LogLevel = LogSeverity.Info,
+                CaseSensitiveCommands = false
+            });
+
+            _client.Log += Logging.Log;
+            _commands.Log += Logging.Log;
+            var services = ConfigureServices();
+            _commands.AddModulesAsync(Assembly.GetEntryAssembly(), services);
+            var commandHandler = new CommandHandler(_client, _commands, services);
+            _client.MessageReceived += commandHandler.HandleCommandAsync;
+        }
+
+        private IServiceProvider ConfigureServices()
+        {
+            var map = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands);
+            return map.BuildServiceProvider();
+        }
+
+        private async Task Start()
+        {
+            Console.WriteLine($"Start with token: {_token}");
+            await _client.LoginAsync(TokenType.Bot, _token);
+            await _client.StartAsync();
+            await _client.DownloadUsersAsync(_client.Guilds);
+            await Task.Delay(Timeout.Infinite);
         }
     }
 }
